0252e2fccc4b1741255ff652f6879c232dabbc0a
Nico Stapelbroek <nstapelbroek@gmail.com>
2015-09-10 19:25:16 +0200

    be more generus with people trying to disable symlinking public by allowing any value other then true

--- bootstrap-application.sh
+++ bootstrap-application.sh
@@ -3,7 +3,8 @@ if [ "x$COMPOSER_COMMAND" != "x" ]; then
   composer $COMPOSER_COMMAND
 fi
 
-if [ "$USE_PUBLIC" != "false" ]; then
+if [ "$USE_PUBLIC" == "true" ]; then
+  echo 'symlinking public folder'
   ln -sfn $(pwd)/public/ /var/www/html
 fi
 
